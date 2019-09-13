18c2a158c508e5baff8712077800bdd6c1ec3956
Черноусов Антон <chernousov.a@special.laximo.local>
2019-09-13 11:42:08 +0700

    Убрал лишний echo

--- run.sh
+++ run.sh
@@ -18,8 +18,35 @@ echo "\$config['des_key'] = 'rcmail-!24ByteDESkey*Str'; " >> /var/www/html/confi
 echo "\$config['plugins'] = array( " >> /var/www/html/config/config.inc.php
 echo "    'archive', " >> /var/www/html/config/config.inc.php
 echo "    'zipdownload', " >> /var/www/html/config/config.inc.php
+echo "    'managesieve', " >> /var/www/html/config/config.inc.php
 echo "); " >> /var/www/html/config/config.inc.php
 echo "\$config['skin'] = 'larry'; " >> /var/www/html/config/config.inc.php
+
+# Конфигурация плагина SIEVE
+echo "<?php"  > /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_port'] = $SIEVE_PORT;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_host'] = '$SIEVE_HOST';"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_auth_type'] = null;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_auth_cid'] = null;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_auth_pw'] = null;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_usetls'] = false;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_conn_options'] = null;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_default'] = '/etc/dovecot/sieve/global';"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_script_name'] = 'managesieve';"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_mbox_encoding'] = 'UTF-8';"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_replace_delimiter'] = '';"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_disabled_extensions'] = array();"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_debug'] = false;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_kolab_master'] = false;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_filename_extension'] = '.sieve';"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_filename_exceptions'] = array();"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_domains'] = array();"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_vacation'] = 0;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_vacation_interval'] = 0;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_vacation_addresses_init'] = false;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_vacation_from_init'] = false;"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_notify_methods'] = array('mailto');"  >> /var/www/html/plugins/managesieve/config.inc.php
+echo "$config['managesieve_raw_editor'] = true;"  >> /var/www/html/plugins/managesieve/config.inc.php
 
 # Запускаем PHP-FPM и Nginx
 /usr/sbin/nginx -g 'daemon off;' &
@@ -40,7 +67,6 @@ while true;
                 echo "Proc ${_proc} is down - EXIT NOW!"
                 kill -9 $PROC_ID
         fi
-        echo $LOGOUT
         done
     sleep 5
     done;
